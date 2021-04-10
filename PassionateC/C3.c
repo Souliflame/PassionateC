bd3289dd12cb699e9a853265d9385bb260de7124
souliflame <souliflame01@gmail.com>
2021-04-10 16:18:15 +0900

    도전! 프로그래밍3 코드 1. 4x4 배열 90도 회전 (진행중) / Set 2d array

--- PassionateC/C3.c
+++ PassionateC/C3.c
@@ -1,16 +1,21 @@
 //도전! 프로그래밍3
 
-void Show2DArray() {
+void Show2DArray(int (*arr)[4]) {
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			printf("%2d ", arr[i][j]);
+		}
+		printf("\n");
+	}
 
+	return;
 }
 /*
 [C3-1] 길이가 4x4인 int형 2차원 배열을 선언
-각 행이 1~4, 5~8, 9~12, 13~16이 되도록 쵝화
+각 행이 1~4, 5~8, 9~12, 13~16이 되도록 초기화
 각 배열의 요소를 90도씩 이동시켜서 결과를 출력하는 프로그램을 작성
 */
-int(*Set4x4Arr(void))[4] {
-
-	int arr[4][4];
+void Set4x4Arr(int arr[][4]){
 
 	for (int i = 0; i < 4; ++i) {
 		for (int j = 0; j < 4; ++j) {
@@ -18,14 +23,20 @@ int(*Set4x4Arr(void))[4] {
 		}
 	}
 
-	return arr;
+	return;
 }
 
-void Rotate90R2DIntArr(int arr[][4], int len) {
+void RotateR90Arr(int arr[][4]) {
+
+	static int arrRotated[4][4];
 
 	for (int i = 0; i < 4; ++i) {
 		for (int j = 0; j < 4; ++j) {
-			
+			arrRotated[i][j] = arr[3 - j][i];
 		}
 	}
+
+	arr = arrRotated;
+
+	return;
 }
